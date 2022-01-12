1bc91af8efc4660e9c770fd83805f01cafcc2b29
taip6494 <palisseauthomas@gmail.com>
2022-01-12 15:16:08 +0400

    création de la fonction note

--- lib/main.dart
+++ lib/main.dart
@@ -22,153 +22,35 @@ class _Song extends StatefulWidget {
 
 class Song extends State<_Song>  {
   late AudioPlayer player;
-
+  notes(Color? c, String n){
+    return
+      Expanded(
+        child: TextButton(
+          onPressed: () {
+            player.setAsset(n);
+            player.play();
+          },
+          child: Container(color: c),
+        ),
+      );
+  }
   @override
   void initState() {
     player = AudioPlayer();
   }
   Widget build(BuildContext context) {
-    double deviceWidth = MediaQuery.of(context).size.width;
-    double deviceHeight = MediaQuery.of(context).size.height;
     return Container(
       child: Center(
         child: Column(
           children: [
-            Expanded(
-            flex: 1,
-            child: Container(
-              width: deviceWidth*1,
-              height: deviceHeight*1,
-              child: TextButton(
-                onPressed: (){
-                 player.setAsset('assets/note1.wav');
-                  player.play();
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.red,
-                ),
-                child: Text(" "
+            notes(Colors.red, 'assets/note1.wav'),
+            notes(Colors.orange, 'assets/note1.wav'),
+            notes(Colors.yellow, 'assets/note1.wav'),
+            notes(Colors.green, 'assets/note1.wav'),
+            notes(Colors.greenAccent, 'assets/note1.wav'),
+            notes(Colors.blue, 'assets/note1.wav'),
+            notes(Colors.pinkAccent, 'assets/note1.wav'),
 
-                ),
-              ),
-            )
-
-        ),
-            Expanded(
-                flex: 1,
-                child: Container(
-                  width: deviceWidth*1,
-                  height: deviceHeight*1,
-                  child: TextButton(
-                    onPressed: (){
-                      player.setAsset('assets/note2.wav');
-                      player.play();
-                    },
-                    style: TextButton.styleFrom(
-                      backgroundColor: Colors.orange,
-                    ),
-                    child: Text(" "
-
-                    ),
-                  ),
-                )
-
-            ),
-        Expanded(
-            flex: 1,
-            child: Container(
-              width: deviceWidth*1,
-              height: deviceHeight*1,
-              child: TextButton(
-                onPressed: (){
-                  player.setAsset('assets/note3.wav');
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.yellow,
-                ),
-                child: Text(" "
-
-                ),
-              ),
-            )
-
-        ),
-        Expanded(
-            flex: 1,
-            child: Container(
-              width: deviceWidth*1,
-              height: deviceHeight*1,
-              child: TextButton(
-                onPressed: (){
-                  player.setAsset('assets/note4.wav');
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.green,
-                ),
-                child: Text(" "
-
-                ),
-              ),
-            )
-
-        ),
-        Expanded(
-            flex: 1,
-            child: Container(
-              width: deviceWidth*1,
-              height: deviceHeight*1,
-              child: TextButton(
-                onPressed: (){
-                  player.setAsset('assets/note5.wav');
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.greenAccent,
-                ),
-                child: Text(" "
-
-                ),
-              ),
-            )
-
-        ),
-            Expanded(
-              flex: 1,
-               child: Container(
-                 width: deviceWidth*1,
-                 height: deviceHeight*1,
-                 child: TextButton(
-                   onPressed: (){
-                     player.setAsset('assets/note6.wav');
-                   },
-                   style: TextButton.styleFrom(
-                     backgroundColor: Colors.blue,
-                   ),
-                   child: Text(" "
-
-                   ),
-                 ),
-               )
-
-            ),
-        Expanded(
-            flex: 1,
-            child: Container(
-              width: deviceWidth*1,
-              height: deviceHeight*1,
-              child: TextButton(
-                onPressed: (){
-                  player.setAsset('assets/note7.wav');
-                },
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.purple,
-                ),
-                child: Text(" "
-
-                ),
-              ),
-            )
-
-        ),
           ],
         ),
       ),
